5e5a6730b67a9b4c448ccc970c80b06c446515d8
Theodore Kruczek <theodore.kruczek@gmail.com>
2024-01-16 21:48:09 -0500

    test: :white_check_mark: fix test config

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,12 @@ const jestConfig = {
   transform: {
     '\\.(js|ts|jsx|tsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: ['node_modules/(?!(ootk-core))'],
   testMatch: ['**/test/**/?(*.)+(spec|test).?(m)[jt]s?(x)'],
   moduleFileExtensions: ['js', 'mjs', 'ts'],
   coverageDirectory: '<rootDir>/coverage',
   moduleDirectories: ['node_modules'],
-  modulePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/sgp4/sgp4prop'],
+  modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
   coverageReporters: ['lcov', 'html', 'text'],
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', '/commonjs/', '/test/', '/scripts/', '/coverage/'],
   globalSetup: '<rootDir>/test/lib/globalSetup.js',
