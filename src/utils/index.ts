9a9b50d4402b24398d920ae4ef0a76a7d19edabf
Theodore Kruczek <theodore.kruczek@gmail.com>
2024-01-16 22:04:03 -0500

    refactor: :recycle: move derivative from ootk-core

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './jacobian';
+export * from './functions';
--- test/utils/functions.test.ts
+++ test/utils/functions.test.ts
@@ -1,6 +1,11 @@
-import { jacobian } from '../../src/main';
+import { derivative, jacobian } from '../../src/main';
 
 // jacobian
 it('should be calculate jacobian', () => {
   expect(jacobian((xs: Float64Array) => xs, 1, new Float64Array([1, 2, 3]))).toMatchSnapshot();
+});
+
+// derivative
+it('should be calculate derivative', () => {
+  expect(derivative((x: number) => x * x, 1)).toMatchSnapshot();
 });
