9dd41607da91e4daffd8456f795239e945493a2d
Theodore Kruczek <theodore.kruczek@gmail.com>
2024-03-20 06:24:53 -0400

    feat: :sparkles: add new catalog sources

--- src/enums/CatalogSource.ts
+++ src/enums/CatalogSource.ts
@@ -2,7 +2,9 @@ export enum CatalogSource {
   USSF = 'USSF',
   CELESTRAK = 'Celestrak',
   UNIV_OF_MICH = 'University of Michigan',
+  CALPOLY = 'CalPoly',
+  NUSPACE = 'NuSpace',
   VIMPEL = 'JSC Vimpel',
   TLE_TXT = 'TLE.txt',
-  EXTRA_JSON = 'extra.json',
+  EXTRA_JSON = 'extra.json'
 }
