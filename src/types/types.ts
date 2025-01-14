9828703c3eef82676a54d06f5ab3adb40f296660
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-01-14 15:59:27 -0500

    feat: :sparkles: add PayloadStatus enum

--- src/types/types.ts
+++ src/types/types.ts
@@ -57,6 +57,30 @@ export interface DetailedSatelliteParams extends SatelliteParams {
   altName?: string;
 }
 
+/*
+ * + Operational
+ * - Nonoperational
+ * P Partially Operational
+ * Partially fulfilling primary mission or secondary mission(s)
+ * B Backup/Standby
+ * Previously operational satellite put into reserve status
+ * S Spare
+ * New satellite awaiting full activation
+ * X Extended Mission
+ * D Decayed
+ * ? Unknown
+ */
+export enum PayloadStatus {
+  OPERATIONAL = '+',
+  NONOPERATIONAL = '-',
+  PARTIALLY_OPERATIONAL = 'P',
+  BACKUP_STANDBY = 'B',
+  SPARE = 'S',
+  EXTENDED_MISSION = 'X',
+  DECAYED = 'D',
+  UNKNOWN = '?'
+}
+
 export interface DetailedSensorParams extends SensorParams {
   /** The country that owns the sensor */
   country?: string;
