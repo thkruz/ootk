b03abb49b0db885cba0b1e7859172090d0e50cf6
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-10-24 06:02:45 -0400

    refactor: :recycle: rename astronomicalUnit to KM_PER_AU and update usages in Sun & SolarRadiationPressure

--- src/body/Sun.ts
+++ src/body/Sun.ts
@@ -33,7 +33,6 @@
 import {
   angularDiameter,
   AngularDiameterMethod,
-  astronomicalUnit,
   AzEl,
   Celestial,
   cKmPerSec,
@@ -42,6 +41,7 @@ import {
   Earth,
   EpochUTC,
   Kilometers,
+  KM_PER_AU,
   Meters,
   MS_PER_DAY,
   RAD2DEG,
@@ -500,7 +500,7 @@ export class Sun {
       rMag * Math.cos(obliq * dtr) * Math.sin(lamEc * dtr),
       rMag * Math.sin(obliq * dtr) * Math.sin(lamEc * dtr),
     );
-    const rMOD = r.scale(astronomicalUnit);
+    const rMOD = r.scale(KM_PER_AU);
     const p = Earth.precession(epoch);
 
     return rMOD
