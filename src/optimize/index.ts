bd14b0bf58e965767d0563e07c29df9897513a3a
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-11-29 16:36:20 -0500

    feat(optimize): :sparkles: add index file to export optimization modules

---
+++ src/optimize/index.ts
@@ -0,0 +1,6 @@
+export { ChebyshevCompressor } from './ChebyshevCompressor';
+export { DownhillSimplex } from './DownhillSimplex';
+export { GoldenSection } from './GoldenSection';
+export { PolynomialRegression } from './PolynomialRegression';
+export { SimpleLinearRegression } from './SimpleLinearRegression';
+
