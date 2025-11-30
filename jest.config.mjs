c275e6b74b8aee1d18e1f44097051180e1a59299
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-11-30 00:08:42 -0500

    chore: :wrench: update coveragePathIgnorePatterns in jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -21,7 +21,7 @@ const jestConfig = {
   moduleDirectories: ['node_modules'],
   modulePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/sgp4/sgp4prop', '<rootDir>/test/sgp4/full-catalog'],
   coverageReporters: ['lcov', 'html', 'text'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', '/commonjs/', '/test/', '/scripts/', '/coverage/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', '/commonjs/', '/test/', '/scripts/', '/coverage/', '/__tests__/'],
   globalSetup: '<rootDir>/test/lib/globalSetup.js',
 };
 
