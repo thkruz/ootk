b1cae93f0617e9db4af2dada989629de52aeb8a1
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-12-06 16:53:47 -0500

    refactor: :recycle: clean up exports in index.ts

--- src/objects/index.ts
+++ src/objects/index.ts
@@ -1,13 +1,15 @@
+export { AccessCalculator } from './AccessCalculator';
+export type { AccessConstraints, AccessWindow } from './AccessWindow';
 export { BaseObject } from './BaseObject';
 export { CenterBody, CenterBodyMu, parseCenterBody } from './CenterBody';
+export { DynamicGroundObject } from './DynamicGroundObject';
+export type { DynamicGroundObjectParams, GroundInterpolationMethod, WaypointData } from './DynamicGroundObject';
 export { EphemerisSatellite } from './EphemerisSatellite';
 export type { EphemerisSatelliteParams } from './EphemerisSatellite';
 export { GroundObject } from './GroundObject';
 export type { GroundObjectParams } from './GroundObject';
 export { GroundStation } from './GroundStation';
 export type { GroundStationParams } from './GroundStation';
-export { DynamicGroundObject } from './DynamicGroundObject';
-export type { DynamicGroundObjectParams, GroundInterpolationMethod, WaypointData } from './DynamicGroundObject';
 export { History } from './History';
 export type { HistoryConfig, HistoryEntry } from './History';
 export { DEFAULT_INTERPOLATOR, DEFAULT_LAGRANGE_ORDER, InterpolatorType } from './InterpolatorType';
@@ -18,7 +20,7 @@ export type {
   HistoricalState,
   PropagatorType,
   SensorInterface,
-  SerializedObject,
+  SerializedObject
 } from './ObjectTypes';
 export { Satellite } from './Satellite';
 export { SpaceObject } from './SpaceObject';
@@ -26,12 +28,4 @@ export type { SpaceObjectParams } from './SpaceObject';
 export { Star } from './Star';
 export { VisualizationHelpers } from './VisualizationHelpers';
 export type { FovBoundaryPoint, GroundTrackPoint, OrbitTrackPoint } from './VisualizationHelpers';
-export { AccessCalculator } from './AccessCalculator';
-export type { AccessConstraints, AccessWindow } from './AccessWindow';
 
-// Backward compatibility - DetailedSatellite is now just Satellite
-// All detailed properties have been merged into Satellite
-/**
- * @deprecated Use Satellite directly - DetailedSatellite properties have been merged into Satellite
- */
-export { Satellite as DetailedSatellite } from './Satellite';
