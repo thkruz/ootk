c23666fcf05fa033e47a0d8b2ec9ba9b0d806e69
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-02-26 07:21:21 -0500

    feat(numerical-propagator): :sparkles: add NumericalPropagatorOptions interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,6 +2,7 @@ export type { BaseObjectParams } from './BaseObjectParams';
 export type { ClassicalElementsParams } from './ClassicalElementsParams';
 export type { EquinoctialElementsParams } from './EquinoctialElementsParams';
 export type { GroundPositionParams } from './GroundPositionParams';
+export type { NumericalPropagatorOptions } from './NumericalPropagatorOptions';
 export type { OptionsParams } from './OptionsParams';
 export type { SatelliteParams } from './SatelliteParams';
 export type { StarObjectParams } from './StarObjectParams';
