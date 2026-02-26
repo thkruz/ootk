20fd9f6230639d702124fdc8c087fbdb5c8b800e
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-02-26 07:21:26 -0500

    feat(enums): :sparkles: add PropagatorType enum with implementations

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -6,6 +6,7 @@ export { FovFrame } from './FovFrame';
 export { FovShape } from './FovShape';
 export { OrbitRegime } from './OrbitRegime';
 export { PassType } from './PassType';
+export { PropagatorType } from './PropagatorType';
 export { SensorType } from './SensorType';
 export { Sgp4OpsMode } from './Sgp4OpsMode';
 export { SunStatus } from './SunStatus';
